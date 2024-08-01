6dbe84423ec8ef4c75b53ad118ff2d8034ac124c
yogita-bisht-u <yogitabisht.4april@gmail.com>
2024-08-01 15:27:00 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,30 @@
+print(1)
+print(2)
+
+#for loop
+name = 'yogita'
+for i in name:
+    print(i)
+    
+#list
+colors = ["red","green"]
+for color in colors:
+        print( color)
+        for i in color:
+    
+            print(i)
+            
+#range 
+for k in range(5):
+    print(k+1)
+for k  in range(1, 2):
+    print(k)
+    
+    
+    for k in range(1, 12, 8):
+        print(k)
+        
+        
+        
+        #while loop
+ 
